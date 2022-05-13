f03d523d6fc69ef6605ff59cc8605468dd46551a
Samraca <samraca81@gmail.com>
2022-05-12 23:20:28 -0500

    Funcionalidad Básica

--- PMP2/Program.cs
+++ PMP2/Program.cs
@@ -6,14 +6,7 @@ namespace PMP2
     {
         static void Main(string[] args)
         {
-            double[] ventas = {10,12,13,16,19,23,26,30,28,18,16,14};
-            /*
-            Random r = new Random();
-            for (int i = 0; i < ventas.Length; i++)
-            {
-                ventas[i] = r.Next(0,30);
-            }
-            */
+            double[] ventas = { 10, 12, 13, 16, 19, 23, 26, 30, 28, 18, 16, 14 };
             
             Pmp nuevaPrediccion = new Pmp(ventas,0.2,0.3,0.5);
 
